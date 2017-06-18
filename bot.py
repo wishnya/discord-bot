0843f5352cc4dbc9c5c9b768cbed9afb0e64db8a
vishnu <matveykrp@yandex.ru>
2017-06-18 11:21:37 +0300

    Test

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ async def quiz():
 @bot.command()
 async def ask(answer: str):
     global currentAnswer
-    print(discord.Member().nick)
+    #print(discord.Member().nick)
     print(discord.Member())
     if answer.lower() == currentAnswer:
         await bot.say('Это правильный ответ!')
