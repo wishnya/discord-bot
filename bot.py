c8e34d9e020c263a7dac0922dec2165d51a9ba0c
vishnu <matveykrp@yandex.ru>
2017-06-21 20:28:27 +0300

    Add emoji in top-list.

--- bot.py
+++ bot.py
@@ -6,9 +6,11 @@ import sqlite3
 
 bot = discord.Client()
 
+
 @bot.event
 async def on_ready():
     print('UP!')
+
 
 # Открытие/создание базы данных, добавдение таблиц, если их нет.
 dbase = sqlite3.connect('discord.db')
@@ -27,18 +29,32 @@ currentQuestion = False
 currentAnswer = False
 
 # Массив с доступными командами
-arr = ', '.join([
+commands = ', '.join([
     'cat',
     'wiki',
     'python',
     'quiz',
     'ask'
 ])
+places = [
+    ':first_place:',
+    ':second_place:',
+    ':third_place:',
+    ':four:',
+    ':five:',
+    ':six:',
+    ':seven:',
+    ':eight',
+    ':nine:',
+    ':ten:'
+]
+
 
 # Функция, отправляющая случайное изображение кошек
 async def cat(msg):
     gif = requests.get('http://thecatapi.com/api/images/get').url
     await bot.send_message(msg.channel, gif)
+
 
 # Функция поиска по сайтам.
 async def search(msg, where):
@@ -59,6 +75,7 @@ async def search(msg, where):
         quest = 'Вы не указали искомое.'
         await bot.send_message(msg.channel, quest)
 
+
 # Функция, посылающая вопрос в чат
 async def quiz(msg):
     if not currentQuestion:
@@ -66,6 +83,7 @@ async def quiz(msg):
         await bot.send_message(msg.channel, "А теперь вопрос: {}".format(currentQuestion))
     else:
         await bot.send_message(msg.channel, currentQuestion)
+
 
 # Фукция принимающая ответ на текущий вопрос
 async def ask(msg):
@@ -93,10 +111,12 @@ async def ask(msg):
             cursor.close()
             setQuestion()
             await bot.send_message(msg.channel, '{} правильно ответил(а) на вопрос и получаете 1 балл.\n'
-                                                'Теперь количество ваших баллов равняется {}.'.format(msg.author.mention, usrScope))
+                                                'Теперь количество ваших баллов равняется {}.'.format(
+                msg.author.mention, usrScope))
             await bot.send_message(msg.channel, 'А теперь новый вопрос: {}'.format(currentQuestion))
         else:
             await bot.send_message(msg.channel, '{}, к сожелению это неправильный ответ.'.format(msg.author.mention))
+
 
 # Список из 10 лидеров викторины
 async def top(msg):
@@ -104,15 +124,37 @@ async def top(msg):
     cursor.execute('SELECT id, scope FROM scopes ORDER BY scope DESC LIMIT 30')
     leaders = cursor.fetchall()
     count = 0
+    result = {}
     for leader in leaders:
-        if count != 10:
-            nick = discord.utils.get(msg.server.members, id=str(leader[0]))
-            scope = leader[1]
-            if nick is not None:
-                count = count + 1
-                await bot.send_message(msg.channel, '{}: {}'.format(nick.name, scope))
-    cursor.close()
+        usr = str(leader[0])
+        scope = leader[1]
+        if count == 0:
+            result[scope] = [usr]
+        else:
+            if scope in result:
+                result[scope].append(usr)
+            else:
+                result[scope] = [usr]
+        count += 1
 
+    count = 0
+    for key in result:
+        if count != len(result):
+            nick = []
+            if len(result[key]) > 1:
+                for id in result[key]:
+                    tmp = discord.utils.get(msg.server.members, id=id)
+                    if tmp is not None:
+                        nick.append(tmp.name)
+            else:
+                tmp = discord.utils.get(msg.server.members, id=result[key])
+                if tmp is not None:
+                    nick.append(tmp.name)
+            scope = key
+            if nick:
+                await bot.send_message(msg.channel, '{} {}: {} балл(ов).'.format(places[count], *nick, scope))
+                count += 1
+    cursor.close()
 
 
 # Функция, которая выбирает случайный вопрос и ответ из текстового файла, а затем записывает выбранное
@@ -138,12 +180,13 @@ def setQuestion(qst='update'):
     dbase.commit()
     cursor.close()
 
+
 # Функция обработки команд в чате.
 @bot.event
 async def on_message(msg):
     if bot.user.id != msg.author.id:
         if msg.content.startswith('!list'):
-            await bot.send_message(msg.channel, arr)
+            await bot.send_message(msg.channel, commands)
 
         elif msg.content.startswith('!cat'):
             await cat(msg)
@@ -176,7 +219,6 @@ if __name__ == '__main__':
         cursor.close()
     else:
         setQuestion('insert')
+
     # Запуск бота
-    bot.run('MzI0MjU1MDEzNTk5NzA3MTM2.DCrbjw.Y5bsLGA8K0qRtw_cB6U3Xh_KJmE')
-
-
+    bot.run('MzI0MjU1MDEzNTk5NzA3MTM2.DCw-CA.aOHfh4nBQOqY-lCI2fYevvlkxug')
